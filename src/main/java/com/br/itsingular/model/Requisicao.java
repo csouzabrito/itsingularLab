1e690fa7572f9ff6bf5531a0af7f75fbfae6346d
dcarneiro@itsingular.com.br <dcarneiro@itsingular.com.br>
2020-03-24 14:31:21 -0300

    Alteracao para tela de abertura de requisicao

--- src/main/java/com/br/itsingular/model/Requisicao.java
+++ src/main/java/com/br/itsingular/model/Requisicao.java
@@ -1,6 +1,7 @@
 package com.br.itsingular.model;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -136,6 +137,19 @@ public class Requisicao implements Serializable {
 	@NotNull(message = "Campo obrigatório")
 	private String observacao;
 
+	@Column(name = "requisitoObrigatorio")
+	@NotNull(message = "Campo obrigatório")
+	private Integer requisitoObrigatorio;
+	
+	@Column(name = "requisitoConhecimento")
+	@NotNull(message = "Campo obrigatório")
+	private Integer requisitoConhecimento;
+
+	@Column(name = "requisitoTempo")
+	@NotNull(message = "Campo obrigatório")
+	private Integer requisitoTempo;
+	
+	
 	
 	public Requisicao() {}
 
@@ -170,6 +184,9 @@ public class Requisicao implements Serializable {
 	 * @param quaisTestes
 	 * @param localAplicacao
 	 * @param observacao
+	 * @param requisitoObrigatorio
+	 * @param requisitoConhecimento
+	 * @param requisitoTempo
 	 */
 	public Requisicao(long id, @NotBlank(message = "Campo obrigatório") String nomeSolicitante,
 			@NotBlank(message = "Campo obrigatório") String dataSolicitacao,
@@ -195,7 +212,10 @@ public class Requisicao implements Serializable {
 			@NotNull(message = "Campo obrigatório") Integer[] clienteProcesso,
 			@NotNull(message = "Campo obrigatório") Integer[] quaisTestes,
 			@NotNull(message = "Campo obrigatório") String localAplicacao,
-			@NotNull(message = "Campo obrigatório") String observacao) {
+			@NotNull(message = "Campo obrigatório") String observacao,
+			@NotNull(message = "Campo obrigatório") Integer requisitoObrigatorio,
+			@NotNull(message = "Campo obrigatório") Integer requisitoConhecimento,
+			@NotNull(message = "Campo obrigatório") Integer requisitoTempo) {
 		super();
 		this.id = id;
 		this.nomeSolicitante = nomeSolicitante;
@@ -227,484 +247,227 @@ public class Requisicao implements Serializable {
 		this.quaisTestes = quaisTestes;
 		this.localAplicacao = localAplicacao;
 		this.observacao = observacao;
+		this.requisitoObrigatorio = requisitoObrigatorio;
+		this.requisitoConhecimento = requisitoConhecimento;
+		this.requisitoTempo = requisitoTempo;
 	}
 
-	/**
-	 * @return the id
-	 */
+
+
 	public long getId() {
 		return id;
 	}
-
-
-	/**
-	 * @param id the id to set
-	 */
 	public void setId(long id) {
 		this.id = id;
 	}
-
-
-	/**
-	 * @return the nomeSolicitante
-	 */
 	public String getNomeSolicitante() {
 		return nomeSolicitante;
 	}
-
-
-	/**
-	 * @param nomeSolicitante the nomeSolicitante to set
-	 */
 	public void setNomeSolicitante(String nomeSolicitante) {
 		this.nomeSolicitante = nomeSolicitante;
 	}
-
-
-	/**
-	 * @return the dataSolicitacao
-	 */
 	public String getDataSolicitacao() {
 		return dataSolicitacao;
 	}
-
-
-	/**
-	 * @param dataSolicitacao the dataSolicitacao to set
-	 */
 	public void setDataSolicitacao(String dataSolicitacao) {
 		this.dataSolicitacao = dataSolicitacao;
 	}
-
-
-	/**
-	 * @return the cliente
-	 */
 	public String getCliente() {
 		return cliente;
 	}
-
-
-	/**
-	 * @param cliente the cliente to set
-	 */
 	public void setCliente(String cliente) {
 		this.cliente = cliente;
 	}
-
-
-	/**
-	 * @return the ramoAtividade
-	 */
 	public String getRamoAtividade() {
 		return ramoAtividade;
 	}
-
-
-	/**
-	 * @param ramoAtividade the ramoAtividade to set
-	 */
 	public void setRamoAtividade(String ramoAtividade) {
 		this.ramoAtividade = ramoAtividade;
 	}
-
-
-	/**
-	 * @return the endereco
-	 */
 	public String getEndereco() {
 		return endereco;
 	}
-
-
-	/**
-	 * @param endereco the endereco to set
-	 */
 	public void setEndereco(String endereco) {
 		this.endereco = endereco;
 	}
-
-
-	/**
-	 * @return the regiao
-	 */
 	public String getRegiao() {
 		return regiao;
 	}
-
-
-	/**
-	 * @param regiao the regiao to set
-	 */
 	public void setRegiao(String regiao) {
 		this.regiao = regiao;
 	}
-
-
-	/**
-	 * @return the pessoaContato
-	 */
 	public String getPessoaContato() {
 		return pessoaContato;
 	}
-
-
-	/**
-	 * @param pessoaContato the pessoaContato to set
-	 */
 	public void setPessoaContato(String pessoaContato) {
 		this.pessoaContato = pessoaContato;
 	}
-
-
-	/**
-	 * @return the cargo
-	 */
 	public String getCargo() {
 		return cargo;
 	}
-
-
-	/**
-	 * @param cargo the cargo to set
-	 */
 	public void setCargo(String cargo) {
 		this.cargo = cargo;
 	}
-
-
-	/**
-	 * @return the telCorporativo
-	 */
 	public String getTelCorporativo() {
 		return telCorporativo;
 	}
-
-
-	/**
-	 * @param telCorporativo the telCorporativo to set
-	 */
 	public void setTelCorporativo(String telCorporativo) {
 		this.telCorporativo = telCorporativo;
 	}
-
-
-	/**
-	 * @return the telCelular
-	 */
 	public String getTelCelular() {
 		return telCelular;
 	}
-
-
-	/**
-	 * @param telCelular the telCelular to set
-	 */
 	public void setTelCelular(String telCelular) {
 		this.telCelular = telCelular;
 	}
-
-
-	/**
-	 * @return the email
-	 */
 	public String getEmail() {
 		return email;
 	}
-
-
-	/**
-	 * @param email the email to set
-	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
-
-	/**
-	 * @return the perfil
-	 */
 	public String getPerfil() {
 		return perfil;
 	}
-
-
-	/**
-	 * @param perfil the perfil to set
-	 */
 	public void setPerfil(String perfil) {
 		this.perfil = perfil;
 	}
-
-
-	/**
-	 * @return the regiaoAtuacao
-	 */
 	public String getRegiaoAtuacao() {
 		return regiaoAtuacao;
 	}
-
-
-	/**
-	 * @param regiaoAtuacao the regiaoAtuacao to set
-	 */
 	public void setRegiaoAtuacao(String regiaoAtuacao) {
 		this.regiaoAtuacao = regiaoAtuacao;
 	}
-
-
-	/**
-	 * @return the vagas
-	 */
 	public String getVagas() {
 		return vagas;
 	}
-
-
-	/**
-	 * @param vagas the vagas to set
-	 */
 	public void setVagas(String vagas) {
 		this.vagas = vagas;
 	}
-
-
-	/**
-	 * @return the cargaHoraria
-	 */
 	public String getCargaHoraria() {
 		return cargaHoraria;
 	}
-
-
-	/**
-	 * @param cargaHoraria the cargaHoraria to set
-	 */
 	public void setCargaHoraria(String cargaHoraria) {
 		this.cargaHoraria = cargaHoraria;
 	}
-
-
-	/**
-	 * @return the previsaoInicio
-	 */
 	public String getPrevisaoInicio() {
 		return previsaoInicio;
 	}
-
-
-	/**
-	 * @param previsaoInicio the previsaoInicio to set
-	 */
 	public void setPrevisaoInicio(String previsaoInicio) {
 		this.previsaoInicio = previsaoInicio;
 	}
-
-
-	/**
-	 * @return the duracaoContrato
-	 */
 	public String getDuracaoContrato() {
 		return duracaoContrato;
 	}
-
-
-	/**
-	 * @param duracaoContrato the duracaoContrato to set
-	 */
 	public void setDuracaoContrato(String duracaoContrato) {
 		this.duracaoContrato = duracaoContrato;
 	}
-
-
-	/**
-	 * @return the renovacao
-	 */
 	public String getRenovacao() {
 		return renovacao;
 	}
-
-
-	/**
-	 * @param renovacao the renovacao to set
-	 */
 	public void setRenovacao(String renovacao) {
 		this.renovacao = renovacao;
 	}
-
-
-	/**
-	 * @return the formaContratacao
-	 */
 	public String getFormaContratacao() {
 		return formaContratacao;
 	}
-
-
-	/**
-	 * @param formaContratacao the formaContratacao to set
-	 */
 	public void setFormaContratacao(String formaContratacao) {
 		this.formaContratacao = formaContratacao;
 	}
-
-
-	/**
-	 * @return the valor
-	 */
 	public String getValor() {
 		return valor;
 	}
-
-
-	/**
-	 * @param valor the valor to set
-	 */
 	public void setValor(String valor) {
 		this.valor = valor;
 	}
-
-
-	/**
-	 * @return the por
-	 */
 	public String getPor() {
 		return por;
 	}
-
-
-	/**
-	 * @param por the por to set
-	 */
 	public void setPor(String por) {
 		this.por = por;
 	}
-
-
-	/**
-	 * @return the horaAberta
-	 */
 	public String getHoraAberta() {
 		return horaAberta;
 	}
-
-
-	/**
-	 * @param horaAberta the horaAberta to set
-	 */
 	public void setHoraAberta(String horaAberta) {
 		this.horaAberta = horaAberta;
 	}
-
-
-	/**
-	 * @return the quantidadeMinimaHoras
-	 */
 	public Integer getQuantidadeMinimaHoras() {
 		return quantidadeMinimaHoras;
 	}
-
-
-	/**
-	 * @param quantidadeMinimaHoras the quantidadeMinimaHoras to set
-	 */
 	public void setQuantidadeMinimaHoras(Integer quantidadeMinimaHoras) {
 		this.quantidadeMinimaHoras = quantidadeMinimaHoras;
 	}
-
-
-	/**
-	 * @return the rhProcesso
-	 */
 	public Integer[] getRhProcesso() {
 		return rhProcesso;
 	}
-
-
-	/**
-	 * @param rhProcesso the rhProcesso to set
-	 */
 	public void setRhProcesso(Integer[] rhProcesso) {
 		this.rhProcesso = rhProcesso;
 	}
-
-
-	/**
-	 * @return the solicitanteProcesso
-	 */
 	public Integer[] getSolicitanteProcesso() {
 		return solicitanteProcesso;
 	}
-
-
-	/**
-	 * @param solicitanteProcesso the solicitanteProcesso to set
-	 */
 	public void setSolicitanteProcesso(Integer[] solicitanteProcesso) {
 		this.solicitanteProcesso = solicitanteProcesso;
 	}
-
-
-	/**
-	 * @return the clienteProcesso
-	 */
 	public Integer[] getClienteProcesso() {
 		return clienteProcesso;
 	}
-
-
-	/**
-	 * @param clienteProcesso the clienteProcesso to set
-	 */
 	public void setClienteProcesso(Integer[] clienteProcesso) {
 		this.clienteProcesso = clienteProcesso;
 	}
-
-
-	/**
-	 * @return the quaisTestes
-	 */
 	public Integer[] getQuaisTestes() {
 		return quaisTestes;
 	}
-
-
-	/**
-	 * @param quaisTestes the quaisTestes to set
-	 */
 	public void setQuaisTestes(Integer[] quaisTestes) {
 		this.quaisTestes = quaisTestes;
 	}
-
-
-	/**
-	 * @return the localAplicacao
-	 */
 	public String getLocalAplicacao() {
 		return localAplicacao;
 	}
-
-
-	/**
-	 * @param localAplicacao the localAplicacao to set
-	 */
 	public void setLocalAplicacao(String localAplicacao) {
 		this.localAplicacao = localAplicacao;
 	}
-
-
-	/**
-	 * @return the observacao
-	 */
 	public String getObservacao() {
 		return observacao;
 	}
-
-
-	/**
-	 * @param observacao the observacao to set
-	 */
 	public void setObservacao(String observacao) {
 		this.observacao = observacao;
 	}
+	public Integer getRequisitoObrigatorio() {
+		return requisitoObrigatorio;
+	}
+	public void setRequisitoObrigatorio(Integer requisitoObrigatorio) {
+		this.requisitoObrigatorio = requisitoObrigatorio;
+	}
+	public Integer getRequisitoConhecimento() {
+		return requisitoConhecimento;
+	}
+	public void setRequisitoConhecimento(Integer requisitoConhecimento) {
+		this.requisitoConhecimento = requisitoConhecimento;
+	}
+	public Integer getRequisitoTempo() {
+		return requisitoTempo;
+	}
+	public void setRequisitoTempo(Integer requisitoTempo) {
+		this.requisitoTempo = requisitoTempo;
+	}
+
+	@Override
+	public String toString() {
+		return "Requisicao [id=" + id + ", nomeSolicitante=" + nomeSolicitante + ", dataSolicitacao=" + dataSolicitacao
+				+ ", cliente=" + cliente + ", ramoAtividade=" + ramoAtividade + ", endereco=" + endereco + ", regiao="
+				+ regiao + ", pessoaContato=" + pessoaContato + ", cargo=" + cargo + ", telCorporativo="
+				+ telCorporativo + ", telCelular=" + telCelular + ", email=" + email + ", perfil=" + perfil
+				+ ", regiaoAtuacao=" + regiaoAtuacao + ", vagas=" + vagas + ", cargaHoraria=" + cargaHoraria
+				+ ", previsaoInicio=" + previsaoInicio + ", duracaoContrato=" + duracaoContrato + ", renovacao="
+				+ renovacao + ", formaContratacao=" + formaContratacao + ", valor=" + valor + ", por=" + por
+				+ ", horaAberta=" + horaAberta + ", quantidadeMinimaHoras=" + quantidadeMinimaHoras + ", rhProcesso="
+				+ Arrays.toString(rhProcesso) + ", solicitanteProcesso=" + Arrays.toString(solicitanteProcesso)
+				+ ", clienteProcesso=" + Arrays.toString(clienteProcesso) + ", quaisTestes="
+				+ Arrays.toString(quaisTestes) + ", localAplicacao=" + localAplicacao + ", observacao=" + observacao
+				+ ", requisitoObrigatorio=" + requisitoObrigatorio + ", requisitoConhecimento=" + requisitoConhecimento
+				+ ", requisitoTempo=" + requisitoTempo + "]";
+	}
+	
 }
