57603cd48e20a03979a96abd3ddf3b2e8cbf4f91
gdomignues@itsingular.com.br <guilherme@itsingular.com.br>
2020-03-25 18:49:44 -0300

    novas implementações

---
+++ src/main/java/com/br/itsingular/repository/EmpresaRepository.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.br.itsingular.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.br.itsingular.entity.Empresa;
+
+/**
+ * @author guilherme
+ *
+ */
+@Repository
+public interface EmpresaRepository extends MongoRepository<Empresa, Long> {}
