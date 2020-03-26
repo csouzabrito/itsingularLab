e9b2ce60fc624e801db148e9bb1964ed7f9943b5
THIAGO CORREA <thiagofarbo@gmail.com>
2020-03-26 14:53:38 -0300

    Ajustes

--- src/main/java/com/br/itsingular/controller/UsuarioController.java
+++ src/main/java/com/br/itsingular/controller/UsuarioController.java
@@ -2,9 +2,7 @@ package com.br.itsingular.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.br.itsingular.model.Login;
