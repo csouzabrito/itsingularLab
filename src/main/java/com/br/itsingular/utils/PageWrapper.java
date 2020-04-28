e0510a7395e84a08471f463fd4093631beebcafb
THIAGO CORREA <thiagofarbo@gmail.com>
2020-04-28 11:05:57 -0300

    Ajustes

--- src/main/java/com/br/itsingular/utils/PageWrapper.java
+++ src/main/java/com/br/itsingular/utils/PageWrapper.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 
+import lombok.Getter;
+
+@Getter
 public class PageWrapper<T> {
 
 	public static final int MAX_PAGE_ITEM_DISPLAY = 5;
